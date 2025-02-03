ed8ca9c81aa9891242db5e10bdda0866767784ee
luke bullock <111459215+TrueGlimGrold@users.noreply.github.com>
2025-02-02 21:11:30 -0700

    Added some html to play with to the main page

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -9,9 +9,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-    <style>
-    @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
-    </style>
+      <head>
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+        </style>
+      </head>
       <body className="">
         <header className="">
           <div className="">
