0e78c3ca2134fb96c2c06320c436e128def06df8
Arnold Sujan Katru <96995931+ASKHOPE@users.noreply.github.com>
2025-02-10 19:04:55 +0530

    rebase entire project

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,53 +1,23 @@
-import Link from "next/link";
-import "../styles/globals.css";
-import { ReactNode } from "react";
-
-export const metadata = {
-  title: "Handcrafted Haven",
-  description: "A curated collection of artisanal and handmade products to enrich your life.",
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
-
 export default function RootLayout({
   children,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-
-      <head>
-        <style>
-        @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
-        </style>
-      </head>
-      <body className="">
-        <header className="">
-          <div className="">
-            <h1 className="roboto title header-title">Handcrafted Haven</h1>
-            <nav className="header-links">
-              <Link href="/" className="inter">Theme ☀️/🌑</Link> {/*This has to be a button / toggle for theme swtiching to day or night*/}
-              <Link href="/HCH-Home/shop/home" className="inter">Home</Link>
-              <Link href="/HCH-Home/shop" className="inter">Shop</Link>
-              <Link href="/login" className="inter">Login🤵</Link>
-            </nav>
-          </div>
-        </header>
-        <main className="">
-          {children}
-        </main>
-        <footer className="">
-          <h1 className="roboto title footer-title">Handcrafted Haven</h1>
-          <div className="footer-bottom">
-            <p className="inter copyright">&copy; Handcrafted Haven {new Date().getFullYear()} | WDD430</p>
-            <p className="inter"> Become A Seller</p>
-            <Link href="/" className="inter">About Us</Link>
-            <Link href="/" className="inter">Theme ☀️/🌑</Link>
-          </div>
-        </footer>
-      </body>
+        <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-
-    // Logged in user
-
   );
 }
--- nextjs-dashboard/app/login/page.tsx
+++ nextjs-dashboard/app/login/page.tsx
@@ -1,19 +1,20 @@
-import { Suspense } from 'react';
-import { Metadata } from 'next';
-import React from 'react';
-// import Link from 'next/link';
-import "@/styles/globals.css";
-
-export const metadata: Metadata = {
-    title: 'Login',
-};
-
+import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
-
+import { Suspense } from 'react';
+ 
 export default function LoginPage() {
   return (
-    <Suspense>
-      <LoginForm />
-    </Suspense>
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <AcmeLogo />
+          </div>
+        </div>
+        <Suspense>
+          <LoginForm />
+        </Suspense>
+      </div>
+    </main>
   );
 }
